553884c31cf266bd38f4322f96ce244e25b05555
Yonathan Setiadi <yonathan.setiadi@taralite.com>
2018-03-05 11:20:09 +0000

    Merged in script-migration (pull request #12)
    script migration for new loan
    
    * script migration for new loan
    
    * check query for run new migration
    
    * adding LOG for Creating VA
    
    * change time of back job into 30 minutes
    
    Approved-by: Yansen Armandau <yansen.armandau@taralite.com>

---
+++ script/va_migration.js
@@ -0,0 +1,71 @@
+const db = require('../db/connection')
+const { generateVa } = require('./../utils/virtualAccount')
+const { create } = require('./../services/virtualAccount')
+
+const getAllAccountDetail = () => ({
+  name: 'get-account-detail',
+  text: `
+ SELECT
+   "A".id, "A".source, "A".source_id, "UD"."firstName", "UD"."lastName", "UD"."phoneNumber"
+ FROM
+   "Accounts" as "A"
+ INNER JOIN
+   "LoanAccounts" as "LA"
+ ON
+   "LA".id = "A".source_id AND "A".source = 'LoanAccount'
+ INNER JOIN
+   "Loans" as "L"
+ ON
+   "L".id = "LA".loan_id
+ INNER JOIN
+   "LoanProposals" as "LP"
+ ON
+   "LP".id = "L".proposal_id
+ INNER JOIN
+   "BorrowerEntities" as "BE"
+ ON
+   "BE".id = "LP".borrower_entity_id
+ INNER JOIN
+   "User" as "U"
+ ON
+   "U".id = "BE".user_id
+ INNER JOIN
+   "UserDetailIndividual" as "UD"
+ ON
+   "UD"."userId" = "U".id
+ `
+})
+
+const getAccountDetail = async () => {
+  accountDetail = await db.query(getAllAccountDetail())
+  return accountDetail.rows
+}
+
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+
+const migrateVa = async () => {
+  getAccountDetail()
+    .then((accounts) => {
+      return accounts.map(account => {
+        const accountVa = generateVa(account.id, account.source, account.phoneNumber)
+        return accountVa.map(va => {
+          va.first_name = account.firstName
+          va.last_name = account.lastName
+          return va
+        })
+      })
+    })
+    .then(async (virtualAccounts) => {
+      await asyncForEach(virtualAccounts, async (vaAccount) => {
+        return await create(vaAccount)
+      })
+    })
+    .then(() => console.log('finish'))
+    .catch(err => console.log(err))
+}
+
+migrateVa()
