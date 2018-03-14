72c19d173f88997678aff8c9f47479f55c8a3880
Yonathan Setiadi <yonathan.setiadi@taralite.com>
2018-03-14 19:06:31 +0700

    create new migration for lender account

--- script/va_migration.js
+++ script/va_migration.js
@@ -2,7 +2,7 @@ const db = require('../db/connection')
 const { generateVa } = require('./../utils/virtualAccount')
 const { create } = require('./../services/virtualAccount')
 
-const getAllAccountDetail = () => ({
+const getAllLoanDetail = () => ({
   name: 'get-account-detail',
   text: `
  SELECT
@@ -36,9 +36,44 @@ const getAllAccountDetail = () => ({
  `
 })
 
-const getAccountDetail = async () => {
-  accountDetail = await db.query(getAllAccountDetail())
-  return accountDetail.rows
+const getAllLenderDetail = () => ({
+  name: 'get-lender-account',
+  text: `
+  SELECT 
+    "A".id, "A".source, "A".source_id, "LA".id AS "lender_account_id", "Detail"."firstName", "Detail"."lastName", "Detail"."phoneNumber" 
+  FROM 
+    "Accounts" "A"
+  INNER JOIN 
+    "LenderAccounts" "LA" 
+  ON 
+    "LA".id = "A"."source_id" AND "A".source = 'LenderAccount'
+  INNER JOIN 
+    "User" 
+  ON 
+    "User".id = "LA"."user_id"
+  INNER JOIN (
+    SELECT 
+      "userId", 'individual'::varchar AS userType, "firstName", "lastName", npwp, details, "phoneNumber"
+    FROM 
+      "UserDetailIndividual"
+    UNION SELECT 
+      "userId", 'institutional'::varchar AS userType, "companyName" AS "firstName", NULL as "lastName", npwp, details, "phoneNumber" 
+    FROM 
+    "UserDetailInstitutionals"
+  ) "Detail" 
+  ON 
+    "User"."userType" = "Detail".userType AND "User".id = "Detail"."userId";
+  `
+})
+
+const getLenderDetail = async () => {
+  lenderAccount = await db.query(getAllLenderDetail())
+  return lenderAccount.rows
+}
+
+const getLoanDetail = async () => {
+  loanDetail = await db.query(getAllLoanDetail())
+  return loanDetail.rows
 }
 
 const asyncForEach = async (array, callback) => {
@@ -47,8 +82,8 @@ const asyncForEach = async (array, callback) => {
   }
 }
 
-const migrateVa = async () => {
-  getAccountDetail()
+const migrateLoan = async () => {
+  getLoanDetail()
     .then((accounts) => {
       return accounts.map(account => {
         const accountVa = generateVa(account.id, account.source, account.phoneNumber)
@@ -69,4 +104,27 @@ const migrateVa = async () => {
     .catch(err => console.log(err))
 }
 
-migrateVa()
+const migrateLender = async () => {
+  getLenderDetail()
+  .then(lenders => {
+    return lenders.map(lender => {
+      const lenderVa = generateVa(lender.id, lender.source, lender.phoneNumber)
+      return lenderVa.map(va => {
+        va.first_name = lender.first_name
+        va.last_name = lender.last_name
+        va.lender_account_id = lender.lender_account_id
+        return va
+      })
+    })
+  })
+  .then(async (virtualAccounts) => {
+    await asyncForEach(virtualAccounts, async (vaAccount) => {
+      return await create(vaAccount)
+    })
+  })
+  .then(() => console.log('finish'))
+  .catch(err => console.log(err))
+}
+
+migrateLoan()
+migrateLender()
