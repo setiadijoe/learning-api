b6da2a49d834d28c494734dc62028b407fab60e7
Yonathan Setiadi <yonathan.setiadi@taralite.com>
2018-04-03 17:23:46 +0700

    virtual account using random number

--- utils/virtualAccount.js
+++ utils/virtualAccount.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const { IDENTIFIER } = require('./../helpers/constant')
 
 const bankPrefix = {
   'permata': process.env.PREFIX_PERMATA,
@@ -8,13 +7,11 @@ const bankPrefix = {
 
 module.exports.generateVa = (accountId, accountSource, phoneNumber) => {
   let virtualAccounts = []
-
+  const additionalVa = Math.random().toString().slice(2, 12)
   Object.keys(bankPrefix).forEach(bank => {
     const prefix = bankPrefix[bank]
-    const primaryVa = prefix + IDENTIFIER[accountSource] + accountId
-    const additionalVa = phoneNumber.slice(primaryVa.length - 16)
 
-    const virtualAccount = primaryVa + additionalVa
+    const virtualAccount = prefix + additionalVa
     virtualAccounts.push({
       virtual_account_id: virtualAccount,
       account_id: accountId,
