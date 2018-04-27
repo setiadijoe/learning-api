811f5f009c6ebbeb46a2ab5cdde891fcc94f4332
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2018-04-27 13:46:26 +0700

    script-email-burst

--- script/send_burst_email.js
+++ script/send_burst_email.js
@@ -3,17 +3,17 @@ const fs = require('fs')
 const path = require('path')
 
 const ElasticMail = require('nodelastic')
-const get_account_id = require('./get_account_id')
+const get_account_id = require('./get_virtual_account_detail')
 
 const sendBurstEmail = () => {
-  return get_account_id.getVA()
+  return get_account_id.getVirtualAccountDetail()
     .then(vaDetails => {
       const client = new ElasticMail(process.env.ELASTIC_API_KEY)
       return vaDetails.map(va_detail => {
         const body = fs.readFileSync('./Mail_template.html', 'UTF-8')
         const body_text = body.replace(/{{FNAME}}/g, va_detail.fullName.toUpperCase())
-          .replace(/{{VACODE1}}/g, va_detail.virtual_account_id[1])
-          .replace(/{{VACODE2}}/g, va_detail.virtual_account_id[0])
+          .replace(/{{VACODE1}}/g, va_detail.virtual_account_1)
+          .replace(/{{VACODE2}}/g, va_detail.virtual_account_2)
 
         return client.send({
           from: 'customer@taralite.com',
